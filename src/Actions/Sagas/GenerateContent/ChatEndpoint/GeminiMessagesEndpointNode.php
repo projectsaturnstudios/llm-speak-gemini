ba758fb05676640c92e774637c8ee6953c7396a8
Angel Gonzalez <usftechacg@gmail.com>
2025-07-05 19:25:58 -0400

    v0.3.0
    Support for text and toolcalls

--- src/Actions/Sagas/GenerateContent/ChatEndpoint/GeminiMessagesEndpointNode.php
+++ src/Actions/Sagas/GenerateContent/ChatEndpoint/GeminiMessagesEndpointNode.php
@@ -4,7 +4,11 @@ namespace LLMSpeak\Google\Actions\Sagas\GenerateContent\ChatEndpoint;
 
 use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Http;
+use LLMSpeak\Schema\Conversation\TextMessage;
+use LLMSpeak\Schema\Conversation\ToolCall;
+use LLMSpeak\Schema\Conversation\ToolResult;
 use ProjectSaturnStudios\PocketFlow\Node;
+use Symfony\Component\VarDumper\VarDumper;
 
 class GeminiMessagesEndpointNode extends Node
 {
@@ -26,8 +30,67 @@ class GeminiMessagesEndpointNode extends Node
      */
     public function exec(mixed $prep_res): mixed
     {
-        //dd(json_encode($prep_res['body']));
-        $response = Http::withHeaders($prep_res['headers'])->post($this->url, $prep_res['body']);
+        $body = $prep_res['body'];
+        foreach($body['contents'] as $idx => $entry)
+        {
+            if($entry instanceof TextMessage)
+            {
+                $body['contents'][$idx] = [
+                    'role' => $entry->role,
+                    'parts' => [
+                        'text' => $entry->content
+                    ]
+                ];
+            }
+            elseif($entry instanceof ToolCall)
+            {
+                $body['contents'][$idx] = [
+                    'role' => 'model',
+                    'parts' => [
+                        'functionCall' => [
+                            'name' => $entry->tool,
+                            'args' => $entry->input,
+                        ]
+                    ]
+                ];
+
+            }
+            elseif($entry instanceof ToolResult)
+            {
+                $body['contents'][$idx] = [
+                    'role' => 'user',
+                    'parts' => [
+                        'functionResponse' => [
+                            'name' => $entry->tool,
+                            'response' => [
+                                'name' => $entry->tool,
+                                'content' => $entry->result
+                            ]
+                        ]
+                    ]
+                ];
+
+            }
+            else
+            {
+                dd(["you're not done", $entry], $body);
+            }
+        }
+
+        $pre_tools = $body['tools'];
+        $body['tools'] = ['functionDeclarations' => []];
+        foreach($pre_tools as $idx => $tool)
+        {
+            $tool = [
+                'name' => $tool['name'],
+                'description' => $tool['description'],
+                'parameters' => $tool['input_schema']
+            ];
+            $body['tools']['functionDeclarations'][] = $tool;
+        }
+
+        $response = Http::withHeaders($prep_res['headers'])->post($this->url, $body);
+        VarDumper::dump(["Gemini Response", $response->json()]);
         return $response->json();
     }
 
